a5635629416f7f890126e8f6a93938c83f2b7200
Sukomal07 <hypersukomal@gmail.com>
2024-06-23 19:38:55 +0530

    update app.tsx

--- apps/react-project/src/App.tsx
+++ apps/react-project/src/App.tsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    const socketConn = new WebSocket("ws://localhost:8080")
+
+    socketConn.onopen = () => {
+      console.log("connected")
+      setSocket(socketConn)
+    }
+
+    socketConn.onmessage = (message) => {
+      setMessage(message.data)
+    }
+
+    return () => {
+      socketConn.close()
+    }
+
+  }, [])
+
+  if (!socket) {
+    return (
+      <div>
+        Connecting to server...
+      </div>
+    )
+  }
   return (
     <div>
-      hello
+      {message}
     </div>
   )
 }
